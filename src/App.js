6b0398719f20c31af66bbe4a5472d55ac753ec12
Steacy Paquette <steacy@live.ca>
2020-03-10 12:26:43 -0400

    adding files for github pages

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import './App.css';
+import { SearchBox } from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor() {
+    super();
+    // Fucking States
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  // Don't need to bind it if we use the arrow function
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state; // Destructuring
+    // Filter the monsters with what we type in the searchBox
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className='App'>
+        <h1>Catz Rolodex</h1>
+        <SearchBox
+          placeholder='Search Weird Catz'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    );
+  }
+}
+export default App;
