81b84fc417c80f82c0f79cd95a69ef27bd62cb3f
Steacy Paquette <steacy@live.ca>
2020-03-10 12:18:05 -0400

    Updates

---
+++ precache-manifest.6934293627ba8660e62c0dc7022ebbca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5323ffdc3b0e2c4263959874f06bc793",
+    "url": "/catz-rolodex/index.html"
+  },
+  {
+    "revision": "95a4c4d6cc85fb22cf77",
+    "url": "/catz-rolodex/static/css/main.bc428814.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/catz-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/catz-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a4c4d6cc85fb22cf77",
+    "url": "/catz-rolodex/static/js/main.496aedd1.chunk.js"
+  },
+  {
+    "revision": "0ca3f599b47241cfc3d4",
+    "url": "/catz-rolodex/static/js/runtime-main.16fbf302.js"
+  }
+]);
